9207ebddeabde545a658d0d6af9fd3e55934e965
Maycom Hoepers <maycomhoepers10@gmail.com>
2021-07-22 21:42:18 -0300

    first commit

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,61 @@
+import incomeImg from "../../assets/income.svg"
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+import { useTransactions } from '../../hooks/useTransactionsContext';
+
+import { Container } from "./style";
+
+export function Summary() {
+
+    const { transactions } = useTransactions();
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraw += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+    },{
+        deposits: 0,
+        withdraw: 0,
+        total: 0,
+    })
+
+    function formatValue(value: number) {
+
+        return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL', 
+        }).format(value);
+    }
+
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="Entradas"/>
+                </header>
+                <strong>{formatValue(summary.deposits)}</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImg} alt="Saídas"/>
+                </header>
+                <strong>- {formatValue(summary.withdraw)}</strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total"/>
+                </header>
+                <strong>{formatValue(summary.total)}</strong>
+            </div>
+        </Container>
+    );
+}
